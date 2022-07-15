cb3a1da831c06f0289ed7c1043575f0921c13fbc
IgorBulakh <bulach.igor@gmail.com>
2022-07-15 22:01:09 +0300

    section store and slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,11 @@
 $(function () {
-  $(".response__slider-inner").slick({
+  $(".slider-inner").slick({
     dots: true,
     arrows: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><svg class="prev-rrow" height="34" width="34"><use xlink:href="images/sprite.svg#prevarrow"></use></svg></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><svg class="next-rrow" height="34" width="34"><use xlink:href="images/sprite.svg#nextarrow"></use></svg></button>',
     fade: true,
     autoplay: true,
     autoplaySpeed: 2000,
