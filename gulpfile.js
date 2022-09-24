83e117b751db810c41bf80556de4ab1485bbb15b
IgorBulakh <bulach.igor@gmail.com>
2022-09-24 13:14:18 +0300

    code fixed on module structure

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const concat = require("gulp-concat");
 const autoprefixer = require("gulp-autoprefixer");
 const uglify = require("gulp-uglify");
 const imagemin = require("gulp-imagemin");
+const rename = require("gulp-rename");
+const nunjucksRender = require("gulp-nunjucks-render");
 const del = require("del");
 const svgSprite = require("gulp-svg-sprite");
 const browserSync = require("browser-sync").create();
@@ -17,10 +19,21 @@ function browsersync() {
   });
 }
 
+function nunjucks() {
+  return src("app/*.njk")
+    .pipe(nunjucksRender())
+    .pipe(dest("app"))
+    .pipe(browserSync.stream());
+}
+
 function styles() {
-  return src("app/scss/style.scss")
+  return src("app/scss/*.scss")
     .pipe(scss({ outputStyle: "compressed" }))
-    .pipe(concat("style.min.css"))
+    .pipe(
+      rename({
+        suffix: ".min",
+      })
+    )
     .pipe(
       autoprefixer({
         overrideBrowserslist: ["last 10 versions"],
@@ -88,6 +101,7 @@ function svgSprites() {
 
 function watching() {
   watch(["app/scss/**/*.scss"], styles);
+  watch(["app/*.njk"], nunjucks);
   watch(["app/js/**/*.js", "!app/js/main.min.js"], scripts);
   watch(["app/**/*.html"]).on("change", browserSync.reload);
   watch(["app/images/icons/*.svg"], svgSprites);
@@ -98,8 +112,16 @@ exports.scripts = scripts;
 exports.browsersync = browsersync;
 exports.watching = watching;
 exports.images = images;
+exports.nunjucks = nunjucks;
 exports.svgSprites = svgSprites;
 exports.cleanDist = cleanDist;
 exports.build = series(cleanDist, images, build);
 
-exports.default = parallel(svgSprites, styles, scripts, browsersync, watching);
+exports.default = parallel(
+  nunjucks,
+  svgSprites,
+  styles,
+  scripts,
+  browsersync,
+  watching
+);
